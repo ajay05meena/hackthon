a435139df8728ef314c8808df3bf6ebddc1cc08d
ajay.meena <ajay.meena@flipkart.com>
2017-06-17 17:17:13 +0530

    No arg constructor required for json desrialization

--- src/main/java/datastore/dao/PostLikeDao.java
+++ src/main/java/datastore/dao/PostLikeDao.java
@@ -2,9 +2,11 @@ package datastore.dao;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class PostLikeDao {
     private String userId;
     private String userName;
