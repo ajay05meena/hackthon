e17291874b9be8faca668d6c1a7cad4e395b8716
ajay.meena <ajay.meena@flipkart.com>
2017-08-25 20:15:44 +0530

    The following warnings have been detected: WARNING: A resource, Resource{/healthcheck, 0 child resources, 0 resource methods, 0 sub-resource locator, 0 method handler classes, 0 method handler instances}, with path /healthcheck is empty

--- webapp/src/main/java/ex/app/resource/AppHeathCheckResource.java
+++ webapp/src/main/java/ex/app/resource/AppHeathCheckResource.java
@@ -1,5 +1,6 @@
 package ex.app.resource;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -8,6 +9,7 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public class AppHeathCheckResource {
 
+    @GET
     public String test(){
         return "App is up";
     }
